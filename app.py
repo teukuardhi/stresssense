5c2bda0e767d00099e1947dccdbf14f6e09c7927
teukuardhi <teukuardhi822@gmail.com>
2025-08-10 13:55:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def tentang():
     return render_template("tentang.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # <-- Railway-friendly
+    app.run(host='0.0.0.0', port=port, debug=True)
